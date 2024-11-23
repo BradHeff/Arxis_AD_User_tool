9890a2bfd97a1c00baf6f3ccf2d6ce05d81a1746
Brad Heffernan <brad.heffernan83@outlook.com>
2024-11-24 06:11:14 +1030

    asd asd

--- git_setup.sh
+++ git_setup.sh
@@ -1,3 +1,20 @@
-#!/usr/bin/bash
+#!/bin/bash
+set -e
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
 
-python3 /usr/lib/Arxis_AD_Tool/Main.pyw
+git init
+git config --global user.name "Brad Heffernan"
+git config --global user.email "brad.heffernan83@outlook.com"
+sudo git config --system core.editor nano
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=25000'
+git config --global push.default simple
+
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
